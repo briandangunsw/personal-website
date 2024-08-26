3283682e6252dd1a40e0392a470677f60b1c0034
Brian Dang <98614116+briandangunsw@users.noreply.github.com>
2024-08-26 14:02:45 +1000

    Update content and icons
    Update content and icons

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 (function () {
-    [...document.querySelectorAll(".control")].forEach(button => {
-        button.addEventListener("click", function() {
+    const controls = document.querySelectorAll(".control");
+    const themeButton = document.querySelector(".theme-btn");
+
+    controls.forEach(control => {
+        control.addEventListener("click", function() {
             document.querySelector(".active-btn").classList.remove("active-btn");
             this.classList.add("active-btn");
+
             document.querySelector(".active").classList.remove("active");
-            document.getElementById(button.dataset.id).classList.add("active");
-        })
+            document.getElementById(this.dataset.id).classList.add("active");
+        });
     });
-    document.querySelector(".theme-btn").addEventListener("click", () => {
+
+    themeButton.addEventListener("click", () => {
         document.body.classList.toggle("light-mode");
-    })
+    });
 })();
